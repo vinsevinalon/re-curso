98df8aa04264cebef54589a259f01c7d31c5c77f
Vinse Vinalon <vinsevinalon@gmail.com>
2024-02-02 15:00:09 +1100

    Add header component and theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Header from "@/components/header";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Re--curso</h2>
-    </main>
+    <>
+      <Header />
+    </>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,49 @@
+"use client"
+// Importing required libraries
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { ModeToggle } from '@/components/theme-toggle';
+
+
+const Header: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const navigateTo = (route: string) => {
+    router.push(route);
+    setOpen(false);
+  };
+
+  return (
+    <header className="p-5 text-black w-full bg-background text-foreground">
+      <div className="max-w-5xl mx-auto flex justify-between">
+        <div className="flex justify-center items-center" onClick={() => navigateTo('/')}>
+          <h1 className="cursor-pointer">Re-curso</h1>
+        </div>
+        <div className="hidden md:flex">
+          <button type="button" onClick={() => router.push('/')} className="mr-5 hover:text-gray-900">
+            Create
+          </button>
+          <button type="button" onClick={() => router.push('/about')} className="mr-5 hover:text-gray-900">
+            Explore
+          </button>
+          <ModeToggle />
+        </div>
+        <div className="md:hidden">
+          <ModeToggle />
+          <button onClick={() => setOpen(!open)} className='ml-2'>
+            {open ? <FaTimes size={24} /> : <FaBars size={24} />}
+          </button>
+        </div>
+      </div>
+      {open && (
+        <div className="md:hidden">
+          <p onClick={() => navigateTo('/')} className="cursor-pointer mx-2">Create</p>
+          <p onClick={() => navigateTo('/about')} className="cursor-pointer mx-2">Explore</p>
+        </div>
+      )}
+    </header>
+  )
+}
+
+export default Header;
